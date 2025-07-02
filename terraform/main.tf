773d0b7b9a46af294b4466d53b1563eee9a05397
Amit Pal <a007pal@gmail.com>
2025-07-02 20:41:49 +0530

    Aws lambda deplyed in vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,7 @@ resource "aws_lambda_function" "micronaut_lambda" {
   source_code_hash = filebase64sha256("property-view-tracker-0.1-lambda.zip")
   runtime = "provided.al2023"
   architectures = ["x86_64"]
+  #publish = true
   vpc_config {
     security_group_ids = [aws_security_group.lambda_sg.id]
     subnet_ids = [aws_subnet.private_subnet.id]
@@ -56,79 +57,18 @@ resource "aws_lambda_function" "micronaut_lambda" {
 }
 }
 
-#Api GateWay Start
-
-resource "aws_apigatewayv2_api" "api" {
-    name = "${var.funtion_name}-api"
-    protocol_type = "HTTP"
-    description = "API for ${var.funtion_name}"
-}
-
-# Api Gateway permission to invoke Lambda
-resource "aws_lambda_permission" "api_gateway" {
-    statement_id = "AllowApiGatewayInvoke"
-    action = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.micronaut_lambda.arn
-    principal = "apigateway.amazonaws.com"
-    source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
-  
-}
-# Api Gateway integration with Lambda
-resource "aws_apigatewayv2_integration" "lambda_intergration" {
-  api_id                 = aws_apigatewayv2_api.api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.micronaut_lambda.invoke_arn
-  integration_method     = "POST" // This is correct for Lambda proxy integration, even for GET routes
-  payload_format_version = "2.0"
-}
-resource "aws_apigatewayv2_authorizer" "auth0_authorizer" {
-  name = "${var.funtion_name}-auth0-authorizer"
-  api_id = aws_apigatewayv2_api.api.id
-  authorizer_type = "JWT"
-  identity_sources = ["$request.header.Authorization"]
-  jwt_configuration {
-    audience = [var.audience]
-    issuer = var.issuer
-  }  
-}
-# resource "aws_apigatewayv2_route" "lambda_route" {
-#   api_id    = aws_apigatewayv2_api.api.id
-#   route_key = "GET /property/{id}"
-#   target    = "integrations/${aws_apigatewayv2_integration.lambda_intergration.id}"
+# resource "aws_lambda_alias" "dev" {
+#   name = "dev"
+#   function_name = aws_lambda_function.micronaut_lambda.function_name
+#   function_version = aws_lambda_function.micronaut_lambda.version
 # }
-resource "aws_apigatewayv2_route" "lambda_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /property/view"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_intergration.id}"
-  authorization_type = "JWT"
-  authorizer_id = aws_apigatewayv2_authorizer.auth0_authorizer.id
-}
 
+# resource "aws_lambda_provisioned_concurrency_config" "micronaut_lambda_pc" {
+#   function_name = aws_lambda_function.micronaut_lambda.function_name
+#   qualifier = aws_lambda_alias.dev.name
+#   provisioned_concurrent_executions = 1
+#     depends_on = [
+#     aws_lambda_alias.dev
+#   ]
+# }
 
-resource "aws_cloudwatch_log_group" "api_gw_logs" {
-  name              = "/aws/apigateway/${var.funtion_name}-api"
-  retention_in_days = 14
-}
-
-resource "aws_apigatewayv2_stage" "api_stage" {
-  api_id      = aws_apigatewayv2_api.api.id
-  name        = var.environment
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw_logs.arn
-    format = jsonencode({
-      requestId      = "$context.requestId"
-      ip             = "$context.identity.sourceIp"
-      requestTime    = "$context.requestTime"
-      httpMethod     = "$context.httpMethod"
-      routeKey       = "$context.routeKey"
-      status         = "$context.status"
-      protocol       = "$context.protocol"
-      responseLength = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      // Note: HTTP API access logs do not support logging the full request body.
-      // To log request/response bodies, you must do so in your Lambda function code.
-    })
-  }
-}
--- terraform/route.tf
+++ terraform/route.tf
@@ -1,22 +1,22 @@
-# Route Table for private subnet
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.lambda_vpc.id
+# # Route Table for private subnet
+# resource "aws_route_table" "private_rt" {
+#   vpc_id = aws_vpc.lambda_vpc.id
 
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
-  }
+#   route {
+#     cidr_block = "10.0.0.0/16"
+#     gateway_id = "local"
+#   }
 
-  tags = {
-    Name = "${var.funtion_name}-private-rt"
-  }
-}
+#   tags = {
+#     Name = "${var.funtion_name}-private-rt"
+#   }
+# }
 
 # Associate private subnet to private route table
-resource "aws_route_table_association" "private_subnet_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
+# resource "aws_route_table_association" "private_subnet_assoc" {
+#   subnet_id      = aws_subnet.private_subnet.id
+#   route_table_id = aws_route_table.private_rt.id
+# }
 
 # resource "aws_route" "private_s3_route" {
 #   route_table_id = aws_route_table.private_rt.id
