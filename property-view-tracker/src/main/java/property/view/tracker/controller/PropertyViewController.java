fd19e1a9cbcd792159d73c3f764d6b734aac94b2
Amit Pal <a007pal@gmail.com>
2025-07-06 21:40:38 +0530

    module created for each secation

--- property-view-tracker/src/main/java/property/view/tracker/controller/PropertyViewController.java
+++ property-view-tracker/src/main/java/property/view/tracker/controller/PropertyViewController.java
@@ -12,11 +12,6 @@ import static property.view.tracker.constant.OperationStatus.SUCCESS;
 @Controller("/property")
 @Slf4j
 public class PropertyViewController {
- /*   @Get("/{id}")
-    public HttpResponse<String> getId(@PathVariable String id) {
-        log.info("The property Id : {}",id);
-        return HttpResponse.ok(id);
-    }*/
     @Post("/view")
     public HttpResponse<ResponseBody> produceViewData(@Body PropertyView propertyView){
         log.info("Property Id {}",propertyView.getPropertyId());
